d216fc16620a3ecb09cc22c5e7b960715fdb2962
romanbatavi <romanbatavi98@gmail.com>
2021-06-07 19:04:55 +0700

    fetch API

--- pages/index.js
+++ pages/index.js
@@ -15,5 +15,17 @@ export default function Home() {
     <SearchBar type='text' placeholder='Search' />
     <Coins />
     </div>
-  )
+  );
+}
+
+export const getServerSideProps = async () => {
+  const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+
+  const filteredCoins = await res.json()
+
+  return {
+    props: {
+      filteredCoins
+    }
+  }
 }
