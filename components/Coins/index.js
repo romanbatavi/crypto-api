7899f09bc21391097f3d4481bb7ed38aa3c65b01
romanbatavi <romanbatavi98@gmail.com>
2021-06-07 19:47:13 +0700

    git percentage and parse price fixeddddd

--- components/Coins/index.js
+++ components/Coins/index.js
@@ -9,6 +9,21 @@ const Coins = ({ name, price, symbol, marketcap, volume, image, priceChange, id 
         </div>
         <div className='coin_data'>
           <p className='coin_price'>${price}</p>
+          <p className='coin_volume'>${volume.toLocaleString()}</p>
+
+          {priceChange < 0 ? (
+            <p className='coin_percent red'>
+              {priceChange.toFixed(2)}%
+            </p>
+          ) : (
+            <p className='coin_percent green'>
+              {priceChange.toFixed(2)}%
+            </p>
+          )}
+
+          <p className='coin_marketcap'>
+            Mkt Cap: ${marketcap.toLocaleString()}
+          </p>
         </div>
       </div>
     </div>
