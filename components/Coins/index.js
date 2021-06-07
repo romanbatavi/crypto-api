4c2f4db45e6b800eda44faa6963c580dc72a4d49
romanbatavi <romanbatavi98@gmail.com>
2021-06-07 19:34:44 +0700

    get name & symbol

--- components/Coins/index.js
+++ components/Coins/index.js
@@ -1,7 +1,13 @@
-const Coins = ({name}) => {
+const Coins = ({ name, price, symbol, marketcap, volume, image, priceChange, id }) => {
   return (
-    <div>
-      <h1>{name}</h1>
+    <div className='coin_container'>
+      <div className='coin_row'>
+        <div className='coin'>
+          <img src={image} alt={name} className='coin_image' />
+            <h1 className='coin_h1'>{name}</h1>
+            <p className='coin_symbol'>{symbol}</p>
+        </div>
+      </div>
     </div>
   )
 }
