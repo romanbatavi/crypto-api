5e1b40d94849d6caead9a92266384b30b5c33bb4
romanbatavi <romanbatavi98@gmail.com>
2021-06-07 19:11:42 +0700

    get id for coins

--- components/CoinList.js
+++ components/CoinList.js
@@ -1,3 +1,19 @@
+import Coins from "./Coins";
+
 export default function CoinList () {
-  return <></>;
+  return (
+  <>
+  {filteredCoins.map(coin => {
+    return (
+      <Coins
+        key={coin.id} name={coin.name}
+        id={coin.id} price={coin.current_price}
+        symbol={coin.symbol} marketcap={coin.market_cap}
+        volume={total_volume} image={coin.image}
+        priceChange={coin.price_change_percentage_24h}
+      />
+    )
+  })}
+  </>
+  )
 }
