3f5fff66acc669677e9372360ab931da687b58ab
romanbatavi <romanbatavi98@gmail.com>
2021-07-27 20:49:53 +0700

    creating module css

--- pages/coin/[id].js
+++ pages/coin/[id].js
@@ -24,3 +24,17 @@ const Coin = ({ coin }) => {
 };
 
 export default Coin
+
+export async function getServerSideProps(context) {
+  const {id} = context.query
+
+  const res = await fetch (`https://api.coingecko.com/api/v3/coins/${id}`)
+
+  const data = await res.json()
+
+  return {
+    props: {
+      coin: data
+    }
+  }
+}
